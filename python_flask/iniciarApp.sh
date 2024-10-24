55e1602fe52e4746fda030e73ea44d54607e48b4
HenriqueAlmeidaLucareli <henrique.lucareli@germinare.org.br>
2024-10-24 08:57:53 -0300

    v3

--- python_flask/iniciarApp.sh
+++ python_flask/iniciarApp.sh
@@ -1,1 +1,2 @@
-python3 -m http.server
+cd /home/ubuntu/Banco_Aws_pipeline/flare-1.0.0      # Muda para o diretório Aws_pipeline
+python3 -m http.server 8000                         # Executa o script Python
